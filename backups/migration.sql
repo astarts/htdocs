3541ead0d7f633e00c2dd2eafebde38e8176150d
Pchianese <paolo_chianese@hotmail.com>
2015-12-26 14:01:07 +0100

    Dump of databases

--- backups/migration.sql
+++ backups/migration.sql
@@ -7,26 +7,6 @@ UPDATE wp_posts SET post_content = REPLACE(post_content, @source_domain, @domain
 UPDATE wp_posts SET guid = REPLACE(guid, @source_domain, @domain_name);
 
 
--- Migrate pre-production to local
-SET @source_domain = '2016.alessandro-chianese.com';
-SET @domain_name = 'galerie2016.local';
-
-UPDATE wp_options SET option_value = REPLACE(option_value, @source_domain, @domain_name);
-UPDATE wp_posts SET post_content = REPLACE(post_content, @source_domain, @domain_name);
-UPDATE wp_posts SET guid = REPLACE(guid, @source_domain, @domain_name);
-
-
-
--- Migrate production to local
-SET @source_domain = 'www.galerie2016.ch';
-SET @domain_name = 'galerie2016.local';
-
-UPDATE wp_options SET option_value = REPLACE(option_value, @source_domain, @domain_name);
-UPDATE wp_posts SET post_content = REPLACE(post_content, @source_domain, @domain_name);
-UPDATE wp_posts SET guid = REPLACE(guid, @source_domain, @domain_name);
-
-
-
 -- Migrate local to production
 SET @source_domain = 'galerie2016.local';
 SET @domain_name = 'www.galerie2016.ch';
@@ -35,6 +15,18 @@ UPDATE wp_options SET option_value = REPLACE(option_value, @source_domain, @doma
 UPDATE wp_posts SET post_content = REPLACE(post_content, @source_domain, @domain_name);
 UPDATE wp_posts SET guid = REPLACE(guid, @source_domain, @domain_name);
 
+
+
+
+
+
+-- Migrate pre-production to local
+SET @source_domain = '2016.alessandro-chianese.com';
+SET @domain_name = 'galerie2016.local';
+
+UPDATE wp_options SET option_value = REPLACE(option_value, @source_domain, @domain_name);
+UPDATE wp_posts SET post_content = REPLACE(post_content, @source_domain, @domain_name);
+UPDATE wp_posts SET guid = REPLACE(guid, @source_domain, @domain_name);
 
 
 -- Migrate pre-production to production
@@ -46,6 +38,16 @@ UPDATE wp_posts SET post_content = REPLACE(post_content, @source_domain, @domain
 UPDATE wp_posts SET guid = REPLACE(guid, @source_domain, @domain_name);
 
 
+
+
+
+-- Migrate production to local
+SET @source_domain = 'www.galerie2016.ch';
+SET @domain_name = 'galerie2016.local';
+
+UPDATE wp_options SET option_value = REPLACE(option_value, @source_domain, @domain_name);
+UPDATE wp_posts SET post_content = REPLACE(post_content, @source_domain, @domain_name);
+UPDATE wp_posts SET guid = REPLACE(guid, @source_domain, @domain_name);
 
 -- Migrate production to pre-production
 SET @source_domain = 'www.galerie2016.ch';
